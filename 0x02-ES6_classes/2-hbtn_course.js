b8f276cce27892a223ef50d8958de413012f4a98
Atang F Mokamogo <atangfmokamogo@outlook.com>
2023-07-27 20:36:08 +0200

    new project commit, added files for tasks 1 - 3

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._lenth;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(anotherName) {
+    if (anotherName && typeof anotherName === 'string') {
+      this._name = anotherName;
+    }
+  }
+
+  set lenth(anotherLength) {
+    if (anotherLength && typeof anotherLength === 'number') {
+      this._length = anotherLength;
+    }
+  }
+
+  set students(newstudents) {
+    if (newstudents && Array.isArray(newstudents)) {
+      this._students = newstudents;
+    }
+  }
+}
