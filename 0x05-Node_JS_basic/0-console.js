020b01134363ffe29404743f55ead88eac3e2594
Atang F Mokamogo <atangfmokamogo@outlook.com>
2023-09-19 07:39:27 +0200

    uploaded project task 0 and 1 for checker

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * logs an output to stdout.
+ * @param {String} text The message to display.
+ * @author Atang Mokamogo 
+ */
+const displayMessage = (text) => {
+    console.log(text);
+  };
+  
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const input = data.trim();
+
+  if (input.length > 0) {
+    console.log(`Your name is: ${input}`);
+
+    console.log('This important software is now closing');
+    process.exit();
+  }
+});
